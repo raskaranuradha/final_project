7758d8be4805c75094e94e7ce91ba228ef6afe5a
Anuradha <anuradharaskar03@gmail.com>
2023-04-13 19:14:27 +1000

    first commit of final project

---
+++ src/components/MealItem.js
@@ -0,0 +1,28 @@
+import { useNavigate } from "react-router-dom"
+
+const MealItem = ({data}) => {
+    console.log(data)
+    const navigate = useNavigate()
+    return (
+        <>
+        {
+            (!data) ? "Item Does Not Found" : data.map(item => {
+                return(
+               
+         
+                <div className="card" key={item.idMeal} >
+                    <img src = {item.strMealThumb} alt="" onClick= {()=> navigate(`/meal/${item.idMeal}`)}/>
+                    <h3>{item.strMeal}</h3>
+                </div>
+               
+                  
+                
+                   
+                )
+            })
+        }
+        </>
+    )
+}
+
+export default MealItem
