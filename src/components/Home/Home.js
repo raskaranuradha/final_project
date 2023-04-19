279d5eb027667e9becf394e9893e72b11e7f9144
Anuradha <anuradharaskar03@gmail.com>
2023-04-19 10:29:16 +1000

    change image source

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,6 @@ const Home = () => {
     return (
         <div className="home">
             <h1>Welcome to the MealDB website</h1>
-            {/* <img src="https://thumbs.dreamstime.com/b/abstract-food-background-top-view-dark-rustic-kitchen-table-wooden-cutting-board-cooking-spoon-frame-banner-137304354.jpg" alt="" /> */}
             <img src="https://cdn.pixabay.com/photo/2014/06/11/17/00/food-366875_1280.jpg" alt=""/>
         </div>
     
